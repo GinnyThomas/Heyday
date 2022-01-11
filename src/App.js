5f6e58731c0a4632774ba54699b0d6b3b9d262e5
Matt Zimmer <zimmermja@gmail.com>
2022-01-11 11:57:44 +0000

    set up RoomForm render function with Hello message and basic css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from "react";
+import RoomForm from "./components/RoomForm/RoomForm.jsx";
+
 function App() {
-  return <div className="App">Hello test</div>;
+  return (
+    <div className="App">
+      <RoomForm />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/RoomForm/RoomForm.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./roomForm.scss";
+
+export default class RoomForm extends React.Component {
+  render() {
+    console.log("RoomForm loaded");
+    return (
+      <div className="RoomForm">
+        Hello, I am the RoomForm!
+      </div>
+    );
+  }
+}
