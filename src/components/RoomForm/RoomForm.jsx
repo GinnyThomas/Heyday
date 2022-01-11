0b68caed27dbb2c6825af4417593e9f057350a19
Matt Zimmer <zimmermja@gmail.com>
2022-01-11 16:38:42 +0000

    navigation is now working for RoomForm

--- src/components/RoomForm/RoomForm.jsx
+++ src/components/RoomForm/RoomForm.jsx
@@ -3,25 +3,28 @@ import "./roomForm.scss";
 import DateBox from "./DateBox";
 import { useParams, useNavigate } from "react-router-dom";
 
-export default class RoomForm extends React.Component {
-  navigate = () => useNavigate();
+// export default class RoomForm extends React.Component {
+const RoomForm = () => {
+  let navigate = useNavigate();
+  let indicator = 0
 
-  render() {
-    console.log("RoomForm loaded");
-    return (
-      <div className="RoomForm">
-        <h1>Calendar 1</h1>
-        <h3>What days work for you?</h3>
-        <div className="DateButtons">
+  console.log("RoomForm loaded");
+  return (
+    <div className="RoomForm">
+      <h1>Calendar 1</h1>
+      <h3>What days work for you?</h3>
+      <p>{indicator}</p>
+      <div className="DateButtons">
           <DateBox boxID={1} />
           <DateBox boxID={2} />
           <DateBox boxID={3} />
           <DateBox boxID={4} />
-        </div>
-        <button>Submit</button>
-        <button onClick={() => { this.navigate("../room");}}>Cancel</button>
       </div>
-    );
-  }
-}
+      <button>Submit</button>
+      <button onClick={() => { navigate("../room");}}>Cancel</button>
+    </div>
+  );
+};
+
+export default RoomForm
 
