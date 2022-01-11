2c111b4acda9018c10be387783e61f5e990979d0
Courtney Stow <courtney@Courtneys-MacBook-Air.local>
2022-01-11 12:29:11 +0000

    Created a datebox element

---
+++ src/components/RoomForm/DateBox.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./dateBox.scss";
+
+export default class DateBox extends React.Component {
+  render() {
+    console.log("DateBox loaded");
+    return (
+      <div className="DateBox">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
--- src/components/RoomForm/RoomForm.jsx
+++ src/components/RoomForm/RoomForm.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import "./roomForm.scss";
+import DateBox from "./DateBox";
 
 export default class RoomForm extends React.Component {
   render() {
     console.log("RoomForm loaded");
     return (
       <div className="RoomForm">
-        Hello, I am the RoomForm!
+        <h1>Calendar 1</h1>
+        <h3>What days work for you?</h3>
+        <div className="DateButtons">
+          <DateBox />
+          <DateBox />
+          <DateBox />
+          <DateBox />
+        </div>
+        <button>Submit</button>
+        <button>Cancel</button>
       </div>
     );
   }
 }
+
