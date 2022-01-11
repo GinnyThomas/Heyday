7059ba7e88651c3b05ca25a735a63918337755ca
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-11 16:48:07 +0100

    routes added between MVP pages

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import './landing.scss'
+import { useParams, useNavigate } from "react-router-dom";
+
 const Landing = () => {
+  let navigate = useNavigate();
+
   return (
     <div className='landing'>
       <div className='landing_title'>
           <h1>Welcome To Anonymeet</h1>
       </div>
         <div>
-            <button type='submit'>Create New Event</button>
+            <button type='submit' onClick={() => { navigate("setup");}}>
+                Create New Event
+            </button>
         </div>
           </div>
-  )
-}
+  );
+};
 
 export default Landing
