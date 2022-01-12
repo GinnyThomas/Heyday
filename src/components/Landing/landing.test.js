87a336def7f0f8cd33c6267ea0970d85a39ca203
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-12 12:08:02 +0100

    Landing component tests passing

--- src/components/Landing/landing.test.js
+++ src/components/Landing/landing.test.js
@@ -4,8 +4,11 @@ import ReactDOM from 'react-dom';
 import App from '../../App';
 
 it("renders App component correctly", () => {
-  // const header = <h1>Welcome to Anonymeet</h1>;
-  // expect(contains(header)).toEqual(true);
   const { getByText } = render(<App />);
   expect(getByText(/Welcome to Anonymeet/i)).toBeInTheDocument();
+});
+
+it("create new event button is present on the page", () => {
+  const { getByRole } = render(<App />);
+  expect(getByRole('button', {name: /Create New Event/i})).toBeInTheDocument();
 });
