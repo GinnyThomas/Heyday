81e17c2e46e14c606c1ea1798a0d41ddcd3d8099
Myoung <mhbae93@gmail.com>
2022-01-12 11:46:23 +0000

    added logic for rendering result

--- src/components/Room/Room.jsx
+++ src/components/Room/Room.jsx
@@ -1,29 +1,53 @@
 import "./room.scss";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
 
 const Room = () => {
-  const users = 4;
   let navigate = useNavigate();
-  
+  const state = {
+    roomID: 1,
+    startDate: 5,
+    endDate: 10,
+    friendCount: 4,
+    friendCurrent: 1,
+    roomFormsRatings: [1,2,3,4],
+  };
+  // const { state } = useLocation();
+
   return (
     <div className="room">
       <h1>Welcome to your Room!</h1>
       <div className="dataContainer">
         <h3>You'll be meeting up between:</h3>
-        <p>1st Feb - 4th Feb</p>
+        <p>
+          {state.startDate} - {state.endDate}
+        </p>
       </div>
       <div className="responseFormContainer">
         <h3>Choose a response to fill out:</h3>
         <div className="container">
-          {Array.from(Array(users).keys()).map((user) => (
-            <button type="button" id={user + 1} onClick={() => { navigate("../room-form");}}>
+          {Array.from(Array(state.friendCount).keys()).map((user) => (
+            <button
+              type="button"
+              id={user + 1}
+              onClick={() => {
+                navigate("../room-form");
+              }}
+            >
               Response {user + 1}
             </button>
           ))}
         </div>
       </div>
       <div className="resultContainer">
-        <h2>Waiting for results .....</h2>
+        {state.roomFormsRatings.length < state.friendCount ? (
+          <h2>Waiting for results .....</h2>
+        ) : (
+          <>
+            <h2>SUCCESS!</h2>
+            <p>The best day for everyone:</p>
+            <p className="result">3rd Feb</p>
+          </>
+        )}
       </div>
     </div>
   );
