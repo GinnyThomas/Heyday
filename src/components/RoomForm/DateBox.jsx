9cac4cc8528cdfb8773c8b8d5342678b8bd61087
Matt Zimmer <zimmermja@gmail.com>
2022-01-12 12:52:28 +0000

    Dateboxes are feeding information on selections back into the Room array

--- src/components/RoomForm/DateBox.jsx
+++ src/components/RoomForm/DateBox.jsx
@@ -3,21 +3,25 @@ import "./dateBox.scss";
 
 const DateBox = (props) => {
 
-  const handleSelect = () => {
+  const handleSelect = (val) => {
     console.log("Attempting select with "+props.index)
-    props.onClick(props.index, 1)
+    props.onClick(props.index, val)
   }
-
-  handleSelect()
 
   return (
     <div className="DateBox">
-      <h4>{props.date}</h4>
-      <p><input type="radio" id="gold" name={`pref${props.index}`} value="no"></input></p>
-      <p><input type="radio" id="silver" name={`pref${props.index}`} value="no"></input></p>
-      <p><input type="radio" id="bronze" name={`pref${props.index}`} value="no"></input></p>
-      <p><input type="radio" id="no" name={`pref${props.index}`} value="no"></input></p>
+      <h4>{props.index}</h4>
+      <p><input type="radio" id="gold" name={`pref${props.index}`} value="no" 
+        onClick={() => handleSelect(3)}></input></p>
+      <p><input type="radio" id="silver" name={`pref${props.index}`} value="no"
+        onClick={() => handleSelect(2)}></input></p>
+      <p><input type="radio" id="bronze" name={`pref${props.index}`} value="no"
+        onClick={() => handleSelect(1)}></input></p>
+      <p><input type="radio" id="no" name={`pref${props.index}`} value="no"
+        onClick={() => handleSelect(0)}></input></p>
     </div>
   );
 }
 export default DateBox;
+
+// onChange={this.updateDescription}
