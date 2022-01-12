f1799bf32996a0bcccb205a6a46c5f130e95f0b3
Ginny <ginnynjon@gmail.com>
2022-01-12 14:17:17 +0000

    attempted to add mockimplementation to test components and routes

--- src/components/RoomSetup/roomSetup.test.js
+++ src/components/RoomSetup/roomSetup.test.js
@@ -1,31 +1,46 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from '../../App';
-import RoomSetup from "./RoomSetup";
+import {RoomSetup} from "./RoomSetup";
+import {MemoryRouter} from "react-router-dom";
 
 // it("renders App component correctly", () => {
 //   const { getByText } = render(<App />);
 //   expect(getByText(/Start Date/i)).toBeInTheDocument();
 // });
+//
+// function renderWithRouter(ui: ReactElement, {
+//                               route = '/',
+//                               history = createMemoryHistory({ initialEntries: [route] }),
+//                           }: Options = {}
+// ) {
+//     const Wrapper: React.App = ({ children }) => (
+//         <Router history={history}>{children}</Router>
+//     );
+//     return {
+//         ...render(ui, { wrapper: Wrapper }), history,
+//     };
+// }
+//   test('user root redirects to room setup', async () => {
+//       const { findByTest, history, findByLabelText } =
+//     renderWithRouter(<RoomSetup />,
+//         {
+//             route: "setup",
+//         })
+//
+//     expect(history.location.pathname).toEqual('setup');
+//   });
 
-function renderWithRouter(ui: ReactElement, {
-                              route = '/',
-                              history = createMemoryHistory({ initialEntries: [route] }),
-                          }: Options = {}
-) {
-    const Wrapper: React.App = ({ children }) => (
-        <Router history={history}>{children}</Router>
-    );
-    return {
-        ...render(ui, { wrapper: Wrapper }), history,
-    };
-}
-  test('user root redirects to room setup', async () => {
-      const { findByTest, history, findByLabelText } =
-    renderWithRouter(<RoomSetup />,
-        {
-            route: "setup",
-        })
+jest.mock('../../components/Landing/Landing');
 
-    expect(history.location.pathname).toEqual('setup');
-  });
+    test('should render roomsetup page for RoomSetup page route', () => {
+        RoomSetup.mockImplementation(() => <div>RoomSetupPageMock</div>);
+
+        render(
+            <MemoryRouter initialEntries={['setup']}>
+                <App/>
+            </MemoryRouter>
+        );
+
+        expect(screen.getByText("RoomSetupPageMock")).toBeInTheDocument();
+    });
