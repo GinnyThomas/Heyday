31d3ce905152d66efb00ff76ba86a96d874f2b1b
Ginny <ginnynjon@gmail.com>
2022-01-13 10:57:13 +0000

    room setup test checking content present passed

--- src/components/RoomSetup/roomSetup.test.js
+++ src/components/RoomSetup/roomSetup.test.js
@@ -1,46 +1,16 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from '../../App';
-import {RoomSetup} from "./RoomSetup";
-import {MemoryRouter} from "react-router-dom";
+import RoomSetup from "./RoomSetup";
+import { MemoryRouter } from "react-router-dom";
+import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import userEvent from '@testing-library/user-event'
 
-// it("renders App component correctly", () => {
-//   const { getByText } = render(<App />);
-//   expect(getByText(/Start Date/i)).toBeInTheDocument();
-// });
-//
-// function renderWithRouter(ui: ReactElement, {
-//                               route = '/',
-//                               history = createMemoryHistory({ initialEntries: [route] }),
-//                           }: Options = {}
-// ) {
-//     const Wrapper: React.App = ({ children }) => (
-//         <Router history={history}>{children}</Router>
-//     );
-//     return {
-//         ...render(ui, { wrapper: Wrapper }), history,
-//     };
-// }
-//   test('user root redirects to room setup', async () => {
-//       const { findByTest, history, findByLabelText } =
-//     renderWithRouter(<RoomSetup />,
-//         {
-//             route: "/setup",
-//         })
-//
-//     expect(history.location.pathname).toEqual('setup');
-//   });
+test("it renders RoomSetup component correctly and date field is present", () => {
+    const { getByText } = render(
+        <MemoryRouter>
+            <RoomSetup />
+        </MemoryRouter>
+    );
 
-// jest.mock('../../components/RoomSetup/RoomSetup');
-
-//     test('should render roomsetup page for RoomSetup page route', () => {
-//         RoomSetup.mockImplementation(() => <div>RoomSetupPageMock</div>);
-
-//         render(
-//             <MemoryRouter initialEntries={['/setup']}>
-//                 <App/>
-//             </MemoryRouter>
-//         );
-
-//         expect(screen.getByText("RoomSetupPageMock")).toBeInTheDocument();
-//     });
+    expect(getByText(/Start Date/i)).toBeInTheDocument();
+})
