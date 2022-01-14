75fc5d0ad83be18b3eb37e40fbea86ee59197a2c
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-01-14 17:10:45 +0100

    Test added to roomSetup.test.js to test that Submit Preferences buttons works

--- src/components/Room/room.test.js
+++ src/components/Room/room.test.js
@@ -3,54 +3,37 @@ import { render, screen } from '@testing-library/react';
 import Room from "./Room";
 import {MemoryRouter, } from "react-router-dom";
 
+
+const roomRender = () => {    
+render(
+    <MemoryRouter initialEntries={[{
+            "pathname": "/room",
+            "search": "",
+            "hash": "",
+            "state": {
+                "roomID": 1,
+                "startDate": "2022-01-12",
+                "endDate": "2022-01-19",
+                "friendCount": "7",
+                "friendCurrent": -1,
+                "roomFormsRatings": [
+                    []
+                ]
+            },
+            "key": "t03o8cuy"
+    }]
+    }>
+        <Room />
+    </MemoryRouter>
+)};
+
 it('renders Room Correctly', () => {
-    render(
-        <MemoryRouter initialEntries={[{
-                "pathname": "/room",
-                "search": "",
-                "hash": "",
-                "state": {
-                    "roomID": 1,
-                    "startDate": "2022-01-12",
-                    "endDate": "2022-01-19",
-                    "friendCount": "4",
-                    "friendCurrent": -1,
-                    "roomFormsRatings": [
-                        []
-                    ]
-                },
-                "key": "t03o8cuy"
-        }]
-        }>
-            <Room />
-        </MemoryRouter>
-    );
+    roomRender();
     expect(screen.getByText(/Welcome to your room!/i)).toBeInTheDocument()
 })
 
 it('renders the correct number of buttons based on friend count', () => {
-    render(
-        <MemoryRouter initialEntries={[{
-                "pathname": "/room",
-                "search": "",
-                "hash": "",
-                "state": {
-                    "roomID": 1,
-                    "startDate": "2022-01-12",
-                    "endDate": "2022-01-19",
-                    "friendCount": "4",
-                    "friendCurrent": -1,
-                    "roomFormsRatings": [
-                        []
-                    ]
-                },
-                "key": "t03o8cuy"
-        }]
-        }>
-            <Room />
-        </MemoryRouter>
-    );
-   
+    roomRender();
     expect(screen.getByRole('button',{name: /Response 4/i})).toBeInTheDocument();
 })
 
--- src/components/RoomSetup/roomSetup.test.js
+++ src/components/RoomSetup/roomSetup.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react';
 import RoomSetup from "./RoomSetup";
 import {MemoryRouter} from "react-router-dom";
 
@@ -11,4 +11,14 @@ test("it renders RoomSetup component correctly and date field is present", () =>
     );
 
     expect(getByText(/Start Date/i)).toBeInTheDocument();
+})
+
+test('Submit Preferences button exists on the page', () => {
+    const Button = ({onClick, children}) => (
+        <button onClick={onClick}>{children}</button>
+      );
+      const handleClick = jest.fn();
+      render(<Button onClick={handleClick}>Submit Preferences</Button>);
+      fireEvent.click(screen.getByText(/Submit Preferences/i));
+      expect(handleClick).toHaveBeenCalledTimes(1);
 })
