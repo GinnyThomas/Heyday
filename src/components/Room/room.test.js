e1f5c1408ea5f5b9d987f96435e73bf884a9717a
88Andybird <anbird88@gmail.com>
2022-01-14 16:00:15 +0000

    Added a test to check correct number of buttons are present in a room upon setup

--- src/components/Room/room.test.js
+++ src/components/Room/room.test.js
@@ -25,5 +25,33 @@ it('renders Room Correctly', () => {
             <Room />
         </MemoryRouter>
     );
-    expect(screen.getByText(/Welcome to your room!/i)).toBeInTheDocument();
+    expect(screen.getByText(/Welcome to your room!/i)).toBeInTheDocument()
 })
+
+it('renders the correct number of buttons based on friend count', () => {
+    render(
+        <MemoryRouter initialEntries={[{
+                "pathname": "/room",
+                "search": "",
+                "hash": "",
+                "state": {
+                    "roomID": 1,
+                    "startDate": "2022-01-12",
+                    "endDate": "2022-01-19",
+                    "friendCount": "4",
+                    "friendCurrent": -1,
+                    "roomFormsRatings": [
+                        []
+                    ]
+                },
+                "key": "t03o8cuy"
+        }]
+        }>
+            <Room />
+        </MemoryRouter>
+    );
+   
+    expect(screen.getByRole('button',{name: /Response 4/i})).toBeInTheDocument();
+})
+
+
