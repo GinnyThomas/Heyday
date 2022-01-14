4e4eaac9f8c18510cd7f9b671ec3a12c0cef2f13
88Andybird <anbird88@gmail.com>
2022-01-14 16:14:30 +0000

    Tidied test formatting

--- src/components/RoomSetup/roomSetup.test.js
+++ src/components/RoomSetup/roomSetup.test.js
@@ -11,14 +11,14 @@ test("it renders RoomSetup component correctly and date field is present", () =>
     );
 
     expect(getByText(/Start Date/i)).toBeInTheDocument();
-})
+});
 
 test('Submit Preferences button exists on the page', () => {
     const Button = ({onClick, children}) => (
         <button onClick={onClick}>{children}</button>
-      );
-      const handleClick = jest.fn();
-      render(<Button onClick={handleClick}>Submit Preferences</Button>);
-      fireEvent.click(screen.getByText(/Submit Preferences/i));
-      expect(handleClick).toHaveBeenCalledTimes(1);
-})
+    );
+    const handleClick = jest.fn();
+    render(<Button onClick={handleClick}>Submit Preferences</Button>);
+    fireEvent.click(screen.getByText(/Submit Preferences/i));
+    expect(handleClick).toHaveBeenCalledTimes(1);
+});
