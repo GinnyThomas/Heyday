a307d83da4bbc168d82d5240e94f3f5f8a9e602a
Myoung <mhbae93@gmail.com>
2022-01-17 15:35:45 +0000

    updated scss styling for RoomSetup to match figma design

--- src/components/RoomSetup/RoomSetup.jsx
+++ src/components/RoomSetup/RoomSetup.jsx
@@ -70,56 +70,65 @@ const RoomSetup = () => {
         <img src="assets/Hamburger_menu.png" alt="Home Button" />
       </div>
       <h1 className="title">Room set-up</h1>
-      <div className="roomsetup_form">
-        <form onSubmit={handleSubmit}>
-          <div className="dateTextContainer">
-            <h2>1. Choose a time range for a meetup</h2>
-            <p>
-              For example, if you want a meetup some time in March 2025, set
-              your start date as 01/03/2025 and your end date as 31/03/2025.
-            </p>
+      <form className="roomsetup_form" onSubmit={handleSubmit}>
+        <div className="formContainer">
+          <div className="dateContainer">
+            <div className="textContainer">
+              <h2>1. Choose a time range for a meetup</h2>
+              <p>
+                For example, if you want a meetup some time in March 2025, set
+                your start date as 01/03/2025 and your end date as 31/03/2025.
+              </p>
+            </div>
+            <div className="inputContainer">
+              <label>
+                Start date: <br />
+                <input
+                  type="date"
+                  placeholder="dd/mm/yyyy"
+                  name="startDate"
+                  value={startDate}
+                  onChange={handleStartDate}
+                ></input>
+              </label>
+              <label className="endLabel">
+                End date: <br />
+                <input
+                  className="endInput"
+                  type="date"
+                  placeholder="dd/mm/yyyy"
+                  name="endDate"
+                  value={endDate}
+                  onChange={handleEndDate}
+                ></input>
+              </label>
+            </div>
           </div>
-          <label>
-            Start date:
-            <input
-              type="date"
-              placeholder="dd/mm/yyyy"
-              name="startDate"
-              value={startDate}
-              onChange={handleStartDate}
-            ></input>
-          </label>
-          <label>
-            End date:
-            <input
-              type="date"
-              placeholder="dd/mm/yyyy"
-              name="endDate"
-              value={endDate}
-              onChange={handleEndDate}
-            ></input>
-          </label>
-          <div className="groupTextContainer">
-            <h2>2. Choose a group size</h2>
-            <p>This is the number of people you want to attend the meetup.</p>
+          <div className="groupContainer">
+            <div className="textContainer">
+              <h2>2. Choose a group size</h2>
+              <p>This is the number of people you want to attend the meetup.</p>
+            </div>
+            <div className="inputContainer">
+              <label>
+                Group Size: <br />
+                <input
+                  type="number"
+                  name="friendCount"
+                  value={friendCount}
+                  onChange={handleFriendCount}
+                ></input>
+              </label>
+            </div>
           </div>
-          <label>
-            Group Size:
-            <input
-              type="number"
-              name="friendCount"
-              value={friendCount}
-              onChange={handleFriendCount}
-            ></input>
-          </label>
-          <button
-            type="submit"
-            // onClick={() => { navigate("../room");}}
-          >
-            Submit
-          </button>
-        </form>
-      </div>
+        </div>
+        <button
+          type="submit"
+          // onClick={() => { navigate("../room");}}
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
