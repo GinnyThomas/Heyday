82babe2590faedcb954f1088ec4bab1e9eaae535
88Andybird <anbird88@gmail.com>
2022-01-17 12:39:55 +0000

    first functions created for logic file

--- src/helpers/calculation.js
+++ src/helpers/calculation.js
@@ -1,25 +1,5 @@
-// const setResult = () => {
-//     const newArr = [];
-//     let total = 0
-//     state.roomFormsRatings.map((ratingsArr) => {
-//       if (ratingsArr.length > 0) {
-//         for (let i = 0; i < ratingsArr.length; i++) {
-//           total += ratingsArr[i];
-//         }
-//         newArr.push(1);
-//       }
-//     });
 
-//     if ((total === 0) && (newArr.length == state.friendCount)) {
-//       return <h2>No one is available on any date! <br></br> Perhaps try different dates?</h2>;
-//     } else if (newArr.length == state.friendCount) {
-//       return calculateRating();
-//     } else {
-//       return <h2>Waiting for results...</h2>;
-//     }
-//   };
-
-const testData = [[0],[0],[0]]
+const testData = [[0,1],[0,1],[0,1]]
 
 const allFormsComplete = (formsArr) => {
     const valueArr = formsArr.map((form)=> form.length)
@@ -27,4 +7,19 @@ const allFormsComplete = (formsArr) => {
     return !valueArr.includes(0) 
 }
 
-console.log(allFormsComplete(testData));
+const everyoneAvailable = (formsArr, dayIndex) => {
+    const available = formsArr.map((form) => form[dayIndex])
+    console.log(`Preferences for day ${dayIndex}: ` + available)
+    return !available.includes(0) 
+}
+
+const availableDays = (formsArr) => {
+
+}
+
+
+
+// console.log(allFormsComplete(testData));
+console.log(everyoneAvailable(testData, 0));
+console.log(everyoneAvailable(testData, 1));
+
