6f316ab3bd89a11c9a739d20008dbd2f891def67
Myoung <mhbae93@gmail.com>
2022-01-19 13:59:23 +0000

    carousel working and centered on page

--- src/components/Landing/Carsouel.jsx
+++ src/components/Landing/Carsouel.jsx
@@ -10,10 +10,10 @@ export default class Carousel extends Component {
     const settings = {
       className: "carousel",
       autoplay: true,
-      autoplaySpeed: 8000,
-      arrows: true,
+      autoplaySpeed: 4000,
       infinite: true,
-      slidesToShow: 3,
+      slidesToShow: 4,
+      slidesToScroll: 1,
       swipeToSlide: true,
       afterChange: function (index) {
         console.log(
@@ -26,36 +26,37 @@ export default class Carousel extends Component {
         <h2 className="title">Developer Team</h2>
         <Slider {...settings}>
           <Card
+            className="card"
             name={"Myoung"}
-            bio="Full Stack Software Developer and ARB Architect"
+            bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
             fullName="Myoung Bae"
             role="Software Developer"
             github="mhbae-dev"
           />
           <Card
             name={"andy"}
-            bio="I'm a logical, driven problem solver. I have a background in accounting,tiling and fictional writing. The innovation, creativity and freedom to explore are what excites me about tech."
+            bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
             fullName="Andy Bird"
             role="Software Developer"
             github="Andybird88"
           />
           <Card
             name={"ryan"}
-            bio="I am a determined and driven aspiring full-stack developer with a background in engineering and geology. "
+            bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
             fullName="Ryan Grimes"
             role="Software Developer"
             github="RPGrimes"
           />
           <Card
             name={"ginny"}
-            bio="I am a Makers' student excited to learn software engineering. I had previously received my MSN-FNP from Grand Canyon University in 2015."
+            bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
             fullName="Ginny Thomas"
             role="Software Developer"
             github="ginnyamazed"
           />
           <Card
             name={"matt"}
-            bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
             fullName="Matt Zimmer"
             role="Software Developer"
             github="Zimmja"
