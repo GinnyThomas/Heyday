fc53c7e275104892456ba12e4d14dc64c5b19d5c
Myoung <mhbae93@gmail.com>
2022-01-20 04:56:00 +0000

    refactor mousemove logic

--- src/helpers/mousemove.js
+++ src/helpers/mousemove.js
@@ -1,11 +1,16 @@
 const parallax = (e) => {
   document.querySelectorAll(".backWave").forEach((layer) => {
     const speed = layer.getAttribute("data-speed");
-    const x = (window.innerWidth - e.pageX * speed) / 100;
-    const y = (window.innerWidth - e.pageY * speed) / 100;
+    // const x = (window.innerWidth - e.pageX * speed) / 100;
+    // const y = (window.innerWidth - e.pageY * speed) / 100;
+
+    // layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    // layer.style.transitionDuration = "1s";
+
+    const x = (e.clientX * speed) / 250;
+    const y = (e.clientY * speed) / 250;
 
     layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
-    layer.style.transitionDuration = "1s";
   });
 };
 
